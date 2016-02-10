651fe5951d4863f5a92d64d5328e2bd50833bccf
Xinjiang Shao <shaoxinjiang@gmail.com>
2016-02-10 17:39:11 -0600

    looks like node 0.12 and 0.10 doesn't support const yet

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 var objectAssign = require('object-assign');
 
+var EMAIL_REGEX = /\s?<?[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+>?\s?/;
+var PHONE_US_REGEX = /\s?(\(?(\d{3})\)?[- \.]?)?(\d{3,})[- \.]?(\d{4})\s?/;
+
 module.exports = function (str, opts) {
 	opts = objectAssign({
 		stripEmail: true,
@@ -12,16 +15,15 @@ module.exports = function (str, opts) {
 	if (typeof str !== 'string') {
 		throw new TypeError('Expected a string');
 	}
-	const emailRegex = /\s?<?[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+>?\s?/;
-	const phoneUSRegex = /\s?(\(?(\d{3})\)?[- \.]?)?(\d{3,})[- \.]?(\d{4})\s?/;
+
 	// replace email address with a redact string
 	if (opts.stripEmail) {
-		str = str.replace(emailRegex, opts.redactString);
+		str = str.replace(EMAIL_REGEX, opts.redactString);
 	}
 
 	// replace phone numbers with a redact string
-	if (opts.stripPhone && phoneUSRegex.test(str)) {
-		str = str.replace(phoneUSRegex, opts.redactString);
+	if (opts.stripPhone && PHONE_US_REGEX.test(str)) {
+		str = str.replace(PHONE_US_REGEX, opts.redactString);
 	}
 
 	return str;
